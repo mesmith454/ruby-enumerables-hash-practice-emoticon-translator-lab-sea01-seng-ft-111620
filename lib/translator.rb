697925910ac6fca5993c4bca2b26058f2e9242cc
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-08 20:33:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,13 @@
-require 'yaml'
+require 'YAML'
 require 'pry'
 
 def load_library(path)
-  yaml.load_file(path)
   final_hash = {}
-  
+  YAML.load_file(path).each do |key, value|
+    final_hash[key] = {}
+    final_hash[key][:english] = value[0]
+    final_hash[key][:japanese] = value[1]
+  end
   final_hash
 end
 
