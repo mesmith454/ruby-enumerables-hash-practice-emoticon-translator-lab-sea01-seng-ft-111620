330d42661f56fc5f19442345485cdc1c54b33d2e
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-08 20:42:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 def get_english_meaning(path, emoticon)
   load_library(path).each do |key, value|
     if value[:japanese] == emoticon
-      puts key
+      return key
     end
   end
   puts "Sorry, that emoticon was not found."
