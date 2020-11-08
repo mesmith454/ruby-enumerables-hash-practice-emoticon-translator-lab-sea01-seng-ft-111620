7d5219f3403284731a1bf12bf6ab0f66979f691f
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-08 20:20:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,26 +1,14 @@
-require "yaml"
-require "pry"
+require 'yaml'
+require 'pry'
 
 def load_library(path)
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-  emoji = YAML.load_file(path)
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
-  end
-  emoji_hash
+  emoticons = yaml.load_file(path)
+  final_hash = {}
+  
+  final_hash
 end
 
-def get_english_meaning(path, emoticon)
-  emoji = load_library(path)
-  if emoji["get_meaning"].has_key?(emoticon)
-    emoji["get_meaning"][emoticon]
-  else
-    return "Sorry, that emoticon was not found"
-  end
-end
+
 
 
 
